787547f24e60bea748286dfe6ebc7c878ff1ce5d
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-11-27 09:26:47 -0300

    Adicionar o arquivo de configuração do banco de dados

---
+++ scripts/config.py
@@ -0,0 +1,20 @@
+import os
+import helper
+import psycopg2 as db
+from dotenv import load_dotenv
+
+def config_database():
+	# Carrega o arquivo .env
+	load_dotenv()
+	db_host = os.getenv('DB_HOST')
+	db_user = os.getenv('DB_USER')
+	db_pass = os.getenv('DB_PASS')
+	db_name = os.getenv('DB_NAME')
+	
+	try:
+		ptr = db.connect(dbname=db_name,user=db_user,password=db_pass,host=db_host)
+		return ptr.cursor()
+	except :
+		helper.info('e','Erro na conexão com o banco de dados. Verifique as configurações e tente novamente .')
+		
+	
