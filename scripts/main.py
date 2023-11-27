2294c1d7739241244b53cbffbf187119f2e0cc66
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-11-27 09:28:50 -0300

    Adicionar o arquivo principal do jogo

---
+++ scripts/main.py
@@ -0,0 +1,16 @@
+from config import config_database 
+
+def menu():
+	print("Seja bem-vindo ao jogo Skyrim")
+	print("Selecione as opções: ")
+	print("1 - Iniciar o jogo\n2 - Sair\n:",end='')
+	opcao = int(input())
+	
+	if opcao == 1:
+		print("Jogando ...")
+	else:
+		print("Até mais ..")
+		exit(0)
+
+print(config_database())		
+#menu()
