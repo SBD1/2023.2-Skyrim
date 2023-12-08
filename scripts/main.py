fe4ae5e12fd82b114b4aae0bf4706ce955cfbb6b
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-12-08 12:12:33 -0300

    feat : Adicionar opcao de resetar o banco

--- scripts/main.py
+++ scripts/main.py
@@ -1,18 +1,22 @@
-from config import config_database 
+from config import config_database,reset_database
 import os 
 
 def menu():
-	print("Seja bem-vindo ao jogo Skyrim")
+	print("Seja bem-vindo ao mini-jogo Skyrim")
 	print("Selecione as opções: ")
-	print("1 - Iniciar o jogo\n2 - Sair\n:",end='')
+	print("1 - Iniciar o jogo\n2 - Resetar o jogo\n3 - Sair\n:",end='')
 	opcao = int(input())
 	
 	if opcao == 1:
 		print("Jogando ...")
+	elif opcao == 2:
+		reset_database()
+		print('Banco de dados resetado ..')
+		menu()
 	else:
 		print("Até mais ..")
 		exit(0)
 
 config_database()
-os.system('clear')
+#os.system('clear')
 menu()
